a26d383b029ed3c1d65845faa2b39a80cf9abec7
スーイツ <Sweets@users.noreply.github.com>
2019-05-11 07:23:58 -0500

    Create init.h

---
+++ src/init.h
@@ -0,0 +1,12 @@
+#ifndef INIT_H
+#define INIT_H
+
+#define shell "/bin/sh"
+#define init "/etc/rc.init"
+#define shutdown "/etc/rc.shutdown"
+
+void execute(char *);
+void handle_signal(int);
+void initialize_system(void);
+
+#endif
