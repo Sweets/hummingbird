f71b22152a6548828f531f01298cc3364724358c
capezotte <carana2099@gmail.com>
2022-11-15 17:01:31 -0300

    Do not let unexpected signals trigger emergency shells

--- src/hummingbird.c
+++ src/hummingbird.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 #include <sys/wait.h>
 
 #include "hummingbird.h"
@@ -19,7 +20,7 @@ void execute(char *path) {
     if (pid == 0)
         execv(path, command);
     else if (pid)
-        waitpid(pid, NULL, 0);
+        do { errno = 0; waitpid(pid, NULL, 0); } while (errno == EINTR);
 }
 
 int main(int argc, char **argv) {
